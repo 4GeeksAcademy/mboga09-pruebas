88959d9a8eb5efea46156ffc32cdc61ea0155235
María José Bogantes Murillo <88169247+mboga09@users.noreply.github.com>
2023-09-08 18:38:53 +0000

    agregar curso

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,53 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Company_admin(db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "admin_id": self.admin_id,
+            # do not serialize the password, its a security breach
+        }
+
+class Course(db.Model):
+    __tablename__ = 'course'
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(200), unique=True, nullable=False)
+    code = db.Column(db.String(200), unique=True, nullable=False)
+    category = db.Column(db.String(200), unique=True, nullable=False)
+    provider = db.Column(db.String(200), unique=True, nullable=False)
+    cost = db.Column(db.String(200), unique=True, nullable=False)
+    description = db.Column(db.String(200), unique=True, nullable=False)
+    modality = db.Column(db.String(200), unique=True, nullable=False)
+    start_date = db.Column(db.String(200), unique=True, nullable=False)
+    finish_date = db.Column(db.String(200), unique=True, nullable=False)
+    contents = db.Column(db.String(200), unique=True, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "course": self.course,
+            "code": self.code,
+            "category": self.category,
+            "cost": self.cost,
+            "description": self.description,
+            "modality": self.modality,
+            "start_date": self.start_date,
+            "finish_date": self.finish_date,
+            "contents": self.contents,
+            # do not serialize the password, its a security breach
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Course
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,66 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/addCourse', methods=["POST"])
+def add_course():
+    body = request.get_json()
+    course = body["course"]
+    code = body["code"]
+    category = body["cadegory"]
+    provider = body["provider"]
+    cost = body["cost"]
+    description = body["description"]
+    modality = body["modality"]
+    start_date = body["start_date"]
+    finish_date = body["finish_date"]
+    contents = body["finish_date"]
+    is_active = True
+    if body is None:
+        raise APIException("Body está vacío", status_code=400)
+    if course is None or course=="":
+        raise APIException("Llene todos los espacios", status_code=400)
+    if code is None or code=="":
+        raise APIException("El nombre es necesario", status_code=400)
+    if category is None or category=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if provider is None or provider=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if cost is None or cost=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if description is None or description=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if modality is None or modality=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if start_date is None or start_date=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if finish_date is None or finish_date=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    if contents is None or contents=="":
+        raise APIException("Los apellidos son necesarios", status_code=400)
+    course = Course.query.filter_by(code=code).first()
+    #se verifica si el curso ya existe en BD
+    if course:
+        raise APIException("El curso ya existe", status_code=400)
+    #debería encriptar el password
+    #print("password sin encriptar:", password)
+    #password = current_app.bcrypt.generate_password_hash(password, 10).decode("utf-8")
+    #print("password con encriptación:", password)
+    new_course = Course(course=course,
+                        code=code,
+                        category=category,
+                        provider=provider,
+                        cost=cost,
+                        description=description,
+                        modality=modality,
+                        start_date=start_date,
+                        finish_date=finish_date,
+                        contents=contents)
+    try:
+        db.session.add(new_course)
+        db.session.commit()
+        return jsonify({"message":"curso añadido"}), 201
+    except Exception as error:
+        print(str(error))
+        return jsonify({"message":"error al añadir el curso en BD"}), 500
+
